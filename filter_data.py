247bf891fdcfa39d5f79b2a886d3321cb37a2be6
riley <rileykjen@gmail.com>
2026-06-09 12:32:37 -0700

    create filter data file

---
+++ filter_data.py
@@ -0,0 +1,9 @@
+import json
+import ijson
+import geopandas as gpd
+
+select_fires = ["palisades", "mountain", "eaton", "franklin", "line", "bridge"]
+
+raw_file = "../POSTFIRE_MASTER_DATA.geojson"
+
+print("test")
