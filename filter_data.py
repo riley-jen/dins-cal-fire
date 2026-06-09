ec582ffdbd8b445a56cf126898f5ee03a965f234
riley <rileykjen@gmail.com>
2026-06-09 13:46:29 -0700

    mini refactor

--- filter_data.py
+++ filter_data.py
@@ -2,7 +2,9 @@ import json
 import ijson
 import geopandas as gpd
 
-# helper functions
+'''
+helper function that creates a dictionary from a list
+'''
 def create_dict(list):
   new_dict = {}
   for item in list:
@@ -12,24 +14,21 @@ def create_dict(list):
 select_incidents_list = ["palisades", "mountain", "eaton", "franklin", "line", "bridge"]
 select_incidents = create_dict(select_incidents_list)
 
+def read_file(raw_file, filter_dict):
+  new_features = []
+  with open(raw_file, 'rb') as f:
+    features = ijson.items(f,'features.item')
 
-select_features = []
+    for feature in features:
+      properties = feature.get('properties',{})
+      incident = properties.get("INCIDENTNAME",'').lower()
 
-raw_file = "../POSTFIRE_MASTER_DATA.geojson"
+      if incident in filter_dict.keys():
+        filter_dict[incident] += 1
+        new_features.append(feature)
 
-print("checkpoint 1")
+filename = "../POSTFIRE_MASTER_DATA.geojson"
+select_features = read_file(filename, select_incidents)
 
-with open(raw_file, 'rb') as f:
-  features = ijson.items(f,'features.item')
-  done = False
-
-  for feature in features:
-    properties = feature.get('properties',{})
-    incident = properties.get("INCIDENTNAME",'').lower()
-
-    if incident in select_incidents.keys():
-      select_features.append(feature)
-      select_incidents[incident] += 1
-
-print("checkpoint 2")
 print(select_incidents)
+
