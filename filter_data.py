c7a96c3adeb89c570ce7290b47551c55506e34f7
riley <rileykjen@gmail.com>
2026-06-09 13:36:00 -0700

    implement filtering

--- filter_data.py
+++ filter_data.py
@@ -2,8 +2,34 @@ import json
 import ijson
 import geopandas as gpd
 
-select_fires = ["palisades", "mountain", "eaton", "franklin", "line", "bridge"]
+# helper functions
+def create_dict(list):
+  new_dict = {}
+  for item in list:
+    new_dict[item] = 0
+  return new_dict
+
+select_incidents_list = ["palisades", "mountain", "eaton", "franklin", "line", "bridge"]
+select_incidents = create_dict(select_incidents_list)
+
+
+select_features = []
 
 raw_file = "../POSTFIRE_MASTER_DATA.geojson"
 
-print("test")
+print("checkpoint 1")
+
+with open(raw_file, 'rb') as f:
+  features = ijson.items(f,'features.item')
+  done = False
+
+  for feature in features:
+    properties = feature.get('properties',{})
+    incident = properties.get("INCIDENTNAME",'').lower()
+
+    if incident in select_incidents.keys():
+      select_features.append(feature)
+      select_incidents[incident] += 1
+
+print("checkpoint 2")
+print(select_incidents)
