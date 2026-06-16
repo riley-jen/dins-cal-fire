7e7ffdc95070676c49d33ac4da3e36dcaa5b3bb9
riley <rileykjen@gmail.com>
2026-06-16 12:16:29 -0700

    add filter by fire function

--- basic_plot.py
+++ basic_plot.py
@@ -17,5 +17,19 @@ plt.subplots_adjust(bottom=0.15)
 # draw default layers
 gdf_base.plot(ax=ax, markersize=15, color="crimson", alpha=0.8, zorder=2)
 ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik, zorder=1)
+
+def show_specific_fire(fire_name):
+  ax.clear()
+
+  fire_gdf = gdf_base[(gdf_base['INCIDENTNAME'].str.lower()) == fire_name]
+    
+  fire_gdf.plot(ax=ax, markersize=15, color="crimson", alpha=0.8, zorder=2)
+  ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik, zorder=1)
+
+  print(len(fire_gdf))
+  
+  plt.draw()
+fires_list = ["palisades", "mountain", "eaton", "franklin", "line", "bridge"]
+show_specific_fire(fires_list[0])
 ax.set_title("california fire")
 plt.show()
