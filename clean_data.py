ca3b8c3c742c2e70bc256a7c8bad09fc9ba333fd
riley <rileykjen@gmail.com>
2026-06-17 15:26:16 -0700

    check all timestamps

--- clean_data.py
+++ clean_data.py
@@ -9,6 +9,21 @@ not to be confused with filter_data.py
 import json
 import geopandas as gpd
 
-filename = "POSTFIRE_FILTERED_DATA.geojson"
-gdf = gpd.read_file(filename)
+filtered_filename = "POSTFIRE_FILTERED_DATA.geojson"
+gdf = gpd.read_file(filtered_filename)
 
+def get_data_timestamps(gdf):
+  timestamps = []
+
+  for i, data in gdf.iterrows():
+    fire_name = data['INCIDENTNAME']
+    time = data['INCIDENTSTARTDATE']
+
+    ts = (fire_name, time)
+
+    if ts not in timestamps:
+      timestamps.append(ts)
+
+  return timestamps
+
+print(get_data_timestamps(gdf))
