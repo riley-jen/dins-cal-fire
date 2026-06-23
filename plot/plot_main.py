aa71b600dd9ac303ae9cb348cf06694521bfe35f
riley <rileykjen@gmail.com>
2026-06-23 16:12:38 -0700

    change map dimensions

--- plot/plot_main.py
+++ plot/plot_main.py
@@ -12,46 +12,26 @@ fig, ax = plt.subplots(figsize=(8, 8))
 plt.subplots_adjust(bottom=0.15)
 
 fires_list = ['palisades', 'mountain', 'eaton', 'franklin', 'line', 'bridge']
-map_position = [0.06, 0.50, 0.44, 0.44] # left, bottom, width, height
+map_position = [0.06, 0.50, 0.66, 0.44] # left, bottom, width, height
 
 # --- helper funcs ---
 # draw plot
 def plot_fire(fire_name):
   ax.clear()
+  set_map_position(ax)
 
   show_fire_structure(ax, fire_name) # zorder 3
   show_fire_perimeter(ax, fire_name) # zorder 2
-  set_square_map_extent(ax)
   ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik, zorder=1)
   
   apply_base_features(fire_name)
   add_scale_bar(ax)
   plt.draw()
 
-# keep the map in a fixed square in the top left corner
-def set_square_map_position(ax):
+# keep the map in the upper-left part of the fixed-size window
+def set_map_position(ax):
   ax.set_position(map_position)
-  ax.set_aspect('equal', adjustable='box')
-
-# keep the plotted map extent square, even if the data is wider or taller
-def set_square_map_extent(ax):
-  x_min, x_max = ax.get_xlim()
-  y_min, y_max = ax.get_ylim()
-
-  x_center = (x_min + x_max) / 2
-  y_center = (y_min + y_max) / 2
-  width = x_max - x_min
-  height = y_max - y_min
-  side_length = max(width, height)
-
-  if side_length == 0:
-    side_length = 1000
-
-  side_length *= 1.1
-  half_side = side_length / 2
-
-  ax.set_xlim(x_center - half_side, x_center + half_side)
-  ax.set_ylim(y_center - half_side, y_center + half_side)
+  ax.set_aspect('auto')
 
 # draw a small scale bar for the current map extent
 def add_scale_bar(ax):
@@ -59,51 +39,17 @@ def add_scale_bar(ax):
   y_min, y_max = ax.get_ylim()
   map_width = x_max - x_min
   map_height = y_max - y_min
-  scale_length = get_scale_length(map_width)
-
+  scale_length = max([length for length in 
+    [30.48, 76.2, 152.4, 304.8, 804.672, 1609.344, 8046.72, 16093.44, 80467.2] 
+    if length <= map_width / 4] or [30.48])
   x_start = x_min + map_width * 0.08
   y_start = y_min + map_height * 0.08
   x_end = x_start + scale_length
+  label = str(int(scale_length / 1609.344)) + ' mi' if scale_length >= 1609.344 else str(int(scale_length / 0.3048)) + ' ft'
 
   ax.plot([x_start, x_end], [y_start, y_start], color='black', linewidth=3, zorder=4)
-  ax.text(
-    (x_start + x_end) / 2,
-    y_start + map_height * 0.02,
-    make_scale_label(scale_length),
-    ha='center',
-    va='bottom',
-    color='black',
-    fontsize=9,
-    fontweight='bold',
-    bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=2),
-    zorder=4
-  )
-
-# choose a readable scale length in meters for the map size
-def get_scale_length(map_width):
-  max_scale_length = map_width / 4
-  scale_options = [
-    30.48, 76.2, 152.4, 304.8, 804.672,
-    1609.344, 8046.72, 16093.44, 80467.2
-  ]
-
-  for scale_length in reversed(scale_options):
-    if scale_length <= max_scale_length:
-      return scale_length
-
-  return scale_options[0]
-
-# format the scale length as feet or miles
-def make_scale_label(scale_length):
-  mile = 1609.344
-  foot = 0.3048
-
-  if scale_length >= mile:
-    miles = scale_length / mile
-    return str(int(miles)) + ' mi'
-
-  feet = scale_length / foot
-  return str(int(feet)) + ' ft'
+  ax.text((x_start + x_end) / 2, y_start + map_height * 0.02, label, ha='center', va='bottom', color='black', 
+    fontsize=9, fontweight='bold', bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=2), zorder=4)
 
 # make buttons
 def make_fire_buttons(buttons, fires_list):
@@ -124,7 +70,7 @@ def make_fire_buttons(buttons, fires_list):
 
 # set basic features for the plot
 def apply_base_features(fire_name = ""):
-  set_square_map_position(ax)
+  set_map_position(ax)
   if fire_name != "":
     ax.set_title('california fire: ' + fire_name)
   else:
