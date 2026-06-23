5d711dc5d7d7a4c873be8c66af7d6ddbd396e683
riley <rileykjen@gmail.com>
2026-06-23 16:36:13 -0700

    reorganize file

--- plot/plot_main.py
+++ plot/plot_main.py
@@ -14,7 +14,7 @@ plt.subplots_adjust(bottom=0.15)
 fires_list = ['palisades', 'mountain', 'eaton', 'franklin', 'line', 'bridge']
 map_position = [0.06, 0.50, 0.66, 0.44] # left, bottom, width, height
 
-# --- helper funcs ---
+# --- MAIN FUNCTIONS ---
 # draw plot
 def plot_fire(fire_name):
   ax.clear()
@@ -28,6 +28,35 @@ def plot_fire(fire_name):
   apply_base_features(fire_name)
   add_scale_bar(ax)
   plt.draw()
+
+# make buttons
+def make_fire_buttons(buttons, fires_list):
+  nf = len(fires_list)
+
+  for i in range(len(fires_list)):
+    fire_name = fires_list[i]
+    
+    space = 0.025
+    width = (1-(0.2+space*(nf-1)))/nf
+    button_space = plt.axes([0.1+(width+space)*i, 0.05, width, 0.05]) # left, bottom, width, height
+    fire_btn = Button(button_space, fire_name)
+    fire_btn.on_clicked(lambda event, name=fire_name: plot_fire(name))
+
+    buttons.append(fire_btn)
+  
+  return buttons
+
+# --- HELPER FUNCTIONS ---
+
+# set basic features for the plot
+def apply_base_features(fire_name = ""):
+  set_map_position(ax)
+  if fire_name != "":
+    ax.set_title('california fire: ' + fire_name)
+  else:
+    ax.set_title('california fire')
+  ax.get_xaxis().set_visible(False)
+  ax.get_yaxis().set_visible(False)
 
 # keep the map in the upper-left part of the fixed-size window
 def set_map_position(ax):
@@ -67,34 +96,8 @@ def add_scale_bar(ax):
   ax.plot([x_start, x_end], [y_start, y_start], color='black', linewidth=3, zorder=4)
   ax.text((x_start + x_end) / 2, y_start + map_height * 0.02, label, ha='center', va='bottom', color='black', 
     fontsize=9, fontweight='bold', bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=2), zorder=4)
-
-# make buttons
-def make_fire_buttons(buttons, fires_list):
-  nf = len(fires_list)
-
-  for i in range(len(fires_list)):
-    fire_name = fires_list[i]
-    
-    space = 0.025
-    width = (1-(0.2+space*(nf-1)))/nf
-    button_space = plt.axes([0.1+(width+space)*i, 0.05, width, 0.05]) # left, bottom, width, height
-    fire_btn = Button(button_space, fire_name)
-    fire_btn.on_clicked(lambda event, name=fire_name: plot_fire(name))
-
-    buttons.append(fire_btn)
-  
-  return buttons
-
-# set basic features for the plot
-def apply_base_features(fire_name = ""):
-  set_map_position(ax)
-  if fire_name != "":
-    ax.set_title('california fire: ' + fire_name)
-  else:
-    ax.set_title('california fire')
-  ax.get_xaxis().set_visible(False)
-  ax.get_yaxis().set_visible(False)
 # ------
+
 buttons = []
 make_fire_buttons(buttons, fires_list)
 
