3508958f6119cb8428d4fd5c5ab1da9613cb5b07
riley <rileykjen@gmail.com>
2026-06-26 15:06:34 -0700

    change size

--- plot/plot_main.py
+++ plot/plot_main.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 from plot_damage_window import make_damage_window
 from plot_material_window import make_material_window
 
-damage_fig = plt.figure(figsize=(10, 8))
+damage_fig = plt.figure(figsize=(8, 8))
 make_damage_window(damage_fig)
 
-material_fig = plt.figure(figsize=(10, 8))
+material_fig = plt.figure(figsize=(8, 8))
 make_material_window(material_fig)
 
 plt.show()
