dcf93452b807ef2e5b1f90cfae7e9e68563ce36c
riley <rileykjen@gmail.com>
2026-06-29 14:21:15 -0700

    move material table to material window

--- plot/damage/plot_damage_window.py
+++ plot/damage/plot_damage_window.py
@@ -5,11 +5,11 @@ import extract_structure_data
 import extract_perimeter_data
 from damage.plot_map import show_fire_map
 from damage.plot_pie import show_damage_pie
-from damage.plot_material import show_fire_material
 
 # --- VARIABLES ---
 fig = None
 buttons = []
+material_plot_callback = None
 fires_list = ['palisades', 'mountain', 'eaton', 'franklin', 'line', 'bridge']
 
 map_position = [0.06, 0.50, 0.66, 0.44] # left, bottom, width, height
@@ -18,23 +18,21 @@ map_ax = None
 pie_position = [0.7, 0.1, 0.24, 0.38]
 pie_ax = None
 
-table_position = [0.06, 0.1, 0.6, 0.38]
-table_ax = None
-
 
 # --- MAIN FUNCTIONS ---
 # draw plots
 def plot_fire(fire_name):
   map_ax.clear()
   pie_ax.clear()
-  table_ax.clear()
 
   structure_data = extract_structure_data.get_data(fire_name)
   perimeter_data = extract_perimeter_data.get_data(fire_name)
 
   show_fire_map(map_ax, structure_data, perimeter_data, map_position)
   show_damage_pie(pie_ax, structure_data)
-  show_fire_material(table_ax, structure_data)
+
+  if material_plot_callback is not None:
+    material_plot_callback(fire_name)
   
   apply_base_features(fire_name)
   plt.draw()
@@ -63,7 +61,6 @@ def apply_base_features(fire_name = ''):
   if fire_name != '':
     map_ax.set_title('california fire: ' + fire_name)
     pie_ax.set_title('damaged structures\ndistribution')
-    table_ax.set_title('structural composition and material', y=0.85)
   else:
     map_ax.set_title('california fire')
 
@@ -72,18 +69,16 @@ def apply_base_features(fire_name = ''):
 
   pie_ax.axis('off')
 
-  table_ax.axis('off')
-
 # --- SET UP ---
 
-def make_damage_window(input_figure):
-  global fig, map_ax, pie_ax, table_ax, buttons
+def make_damage_window(input_figure, input_material_plot_callback = None):
+  global fig, map_ax, pie_ax, buttons, material_plot_callback
   fig = input_figure
+  material_plot_callback = input_material_plot_callback
 
   fig.canvas.manager.set_window_title('Damage Window')
   map_ax = fig.add_axes(map_position)
   pie_ax = fig.add_axes(pie_position)
-  table_ax = fig.add_axes(table_position)
   
   buttons = []
   make_fire_buttons(buttons, fires_list)
--- plot/material/plot_material_window.py
+++ plot/material/plot_material_window.py
@@ -1,17 +1,50 @@
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+
+import extract_structure_data
+from material.plot_material import show_fire_material
 
 
 # --- VARIABLES ---
 fig = None
 material_ax = None
+buttons = []
+fires_list = ['palisades', 'mountain', 'eaton', 'franklin', 'line', 'bridge']
+
+table_position = [0.06, 0.1, 0.6, 0.38]
+
+
+# --- MAIN FUNCTIONS ---
+
+def plot_fire_material(fire_name):
+  material_ax.clear()
+
+  structure_data = extract_structure_data.get_data(fire_name)
+  show_fire_material(material_ax, structure_data)
+
+  apply_base_features(fire_name)
+  plt.draw()
+
+
+# --- HELPER FUNCTIONS ---
+
+def apply_base_features(fire_name = ''):
+  if fire_name != '':
+    material_ax.set_title('structural composition and material', y=0.85)
+
+  material_ax.axis('off')
 
 
 # --- SET UP ---
 
 def make_material_window(input_figure):
-  global fig, material_ax
+  global fig, material_ax, buttons
   fig = input_figure
 
   fig.canvas.manager.set_window_title('Material Window')
-  material_ax = fig.add_axes([0.08, 0.08, 0.84, 0.84])
-  material_ax.axis('off')
+  material_ax = fig.add_axes(table_position)
+  
+  buttons = []
+  make_fire_buttons(buttons, fires_list)
+
+  apply_base_features()
