1417563405249681ba278dea7c085036a08182d9
riley <rileykjen@gmail.com>
2026-07-02 12:59:16 -0700

    edit pie legend

--- plot/plot_main.py
+++ plot/plot_main.py
@@ -31,7 +31,7 @@ structure_element_axes = {}
 damage_map_position = [0.03, 0.50, 0.33, 0.44]
 damage_pie_position = [0.35, 0.1, 0.12, 0.38]
 damage_sampling_position = [0.075, 0.15, 0.27, 0.28]
-pie_legend_anchor = [0.48, 0.5]
+pie_legend_anchor = [0.37, 0.5]
 
 table_position = [0.55, 0.465, 0.4, 0.34]
 material_bar_position = [0.525, 0.2, 0.275, 0.28]
